80bce8c74c7ae3a47fee2b3ef61752b9cb0cb902
Vijay Patnana <vijaypatnan@gmail.com>
2017-04-30 22:54:17 -0700

    adds unit tests,code coverge

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -2,21 +2,37 @@
 # Cookbook Name:: workstation
 # Spec:: default
 #
-<<<<<<< HEAD
 # Copyright (c) 2017 The Authors, All Rights Reserved.
-=======
-# Copyright (c) 2016 The Authors, All Rights Reserved.
->>>>>>> bf32f68c34747d90c7a65701f63bc51e0b1120ac
 
 require 'spec_helper'
 
 describe 'workstation::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
+    
+    it 'installs the tree package' do
+        expect(chef_run).to install_package('tree')
+    end
 
+    it 'installs the git package' do
+        expect(chef_run).to install_package('git')
+    end
+
+    it 'installs the vim-enhanced package' do
+        expect(chef_run).to install_package('vim-enhanced')
+    end
+    
+    it 'creates motd file' do
+        expect(chef_run).to create_template('/etc/motd')
+    end
+
+    it 'installs the ntp package' do
+        expect(chef_run).to install_package('ntp')
+    end
+    
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
